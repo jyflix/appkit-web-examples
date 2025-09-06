544d3b7c5a75118ad0265d3a744e4034cb2f9172
jyflix <214240557+jyflix@users.noreply.github.com>
2025-09-06 14:50:10 +0530

    Delete javascript directory

--- javascript/javascript-core/src/services/wallet.js
+++
@@ -1,8 +0,0 @@
-export const signMessage = (provider, address) => {
-    if (!provider) return Promise.reject('No provider available')
-    
-    return provider.request({
-      method: 'personal_sign',
-      params: ['Hello from AppKit!', address]
-    })
-  }
